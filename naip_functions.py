62bc6b2b4681f7a9a25f7413425761181289c9b5
David Bailey <31379679+davidjbailey22@users.noreply.github.com>
2019-02-25 23:07:21 -0700

    initial commit

---
+++ naip_functions.py
@@ -0,0 +1,14 @@
+"""
+functions for working with naip imagery
+By D. Bailey
+"""
+
+import gdal, ogr, os, osr
+import numpy
+
+def file_processing():
+    print "dfd"
+
+
+def naip2geotiff():
+    print "dfd"
